334d5d893453e8b4a62fb1c4d120c3063ea1fc4b
apachqq <work.tsyganov@mail.ru>
2022-05-07 10:21:28 +0300

    let-const

---
+++ index.js
@@ -0,0 +1,11 @@
+let a = ''; // first number
+let b = ''; // second number
+let sign = ''; // operation
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+
+// display
+const out = document.querySelector('.calc-screen p');
+
